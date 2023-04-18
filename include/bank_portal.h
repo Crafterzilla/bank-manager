dfda599df562b53336d6a858ee532a2eeaa8e7d5
Osmar Rojas <osmarrojas69@gmail.com>
2023-04-18 15:54:44 -0400

    Add withdraw function

--- include/bank_portal.h
+++ include/bank_portal.h
@@ -7,16 +7,28 @@
 #include "time_time.h"
 #include "input.h"
 
+enum account_type {CHECKING = 1, SAVINGS};
+
 typedef struct User_Bank_Data {
     User user;
     double checking_account_amount;
     double savings_account_amount;
-    char **transaction_histroy;
+    FILE* transaction_fptr;
 } User_Bank_Data;
 
+//Basic banking portal back-end
 void bank_portal(int ID);
 User_Bank_Data init_bank_data(int ID);
 User_Bank_Data null_bank();
+FILE* transaction_fptr(int ID, const char*);
 void store_bank_data(User_Bank_Data*);
+
+//Options in file
+void withdraw(User_Bank_Data*);
+void deposit(User_Bank_Data*);
+void transfer(User_Bank_Data*);
+void check_account_value(User_Bank_Data*);
+void print_account_data(User_Bank_Data*);
+void print_transaction_history(User_Bank_Data*, int lines);
 
 #endif
--- src/bank_portal.c
+++ src/bank_portal.c
@@ -4,26 +4,26 @@ void bank_portal(int ID) {
     User_Bank_Data bdata = init_bank_data(ID);
     Date date = get_current_date(); 
 
-    printf("Welcome %s to Gull and Bull Bank Portal\n", bdata.user.first_name);
+    printf("\nWelcome %s to Gull and Bull Bank Portal\n", bdata.user.first_name);
     printf("Date: %d/%d/%d\n", date.month, date.day, date.year);
 
     int choice = 0;
-    enum options {WITHDRAW = 1, DEPOSIT, TRANSFER, CHECK_ACCOUNTS, VIEW_DATA, LOGOUT};
+    enum options {WITHDRAW = 1, DEPOSIT, TRANSFER, CHECK_ACCOUNTS, VIEW_DATA, VIEW_HISTORY, LOGOUT};
     while (choice != LOGOUT) {
-        puts("Banking Options: ");
+        puts("\nBanking Options: ");
         puts("1.) Withdraw money from account");
         puts("2.) Deposit money from account");
         puts("3.) Check money in accounts");
         puts("4.) Transfer money between accounts");
         puts("5.) View personal information");
-        puts("6.) Logout");
+        puts("6.) View Transaction History");
+        puts("7.) Logout");
 
         printf("Type in an choice from %d to %d: ", WITHDRAW, LOGOUT);
         choice = get_int("Not a vaild integer. Try again: ");
 
         switch (choice) {
-            case WITHDRAW:
-                break;
+            case WITHDRAW: withdraw(&bdata); break;
             case DEPOSIT:
                 break;
             case TRANSFER:
@@ -31,6 +31,8 @@ void bank_portal(int ID) {
             case CHECK_ACCOUNTS:
                 break;
             case VIEW_DATA:
+                break;
+            case VIEW_HISTORY:
                 break;
             case LOGOUT:
                 break;
@@ -45,6 +47,23 @@ void bank_portal(int ID) {
 
 User_Bank_Data null_bank() {
     return (User_Bank_Data){return_null_user(), 0, 0, NULL};
+}
+
+FILE* transaction_fptr(int ID, const char* mode) {
+    //Get str for where the transcations are stored for user ID
+    char filename[100] = {0};
+    sprintf(filename, "./data/logs/%d_log.txt", ID);
+
+    //Attempt to open file. If success full, return file,
+    //else create file, and reopen file in mode and return it 
+    FILE* transaction_file = fopen(filename, mode);
+    if (!transaction_file) {
+        transaction_file = fopen(filename, "w");
+        fclose(transaction_file);
+        transaction_file = fopen(filename, mode);
+    }
+
+    return transaction_file;
 }
 
 User_Bank_Data init_bank_data(int ID) {
@@ -68,13 +87,11 @@ User_Bank_Data init_bank_data(int ID) {
     bdata.savings_account_amount = strtod(double_str, &tmp);
     free(double_str);
 
-    bdata.transaction_histroy = NULL;
+    bdata.transaction_fptr = transaction_fptr(ID, "r+");
 
     fclose(fptr); //Close file
     return bdata; //Return all the data
 }
-
-
 
 void store_bank_data(User_Bank_Data* bdata) {
     int ID = bdata->user.ID;
@@ -85,4 +102,44 @@ void store_bank_data(User_Bank_Data* bdata) {
 
     fclose(fptr);
     free_user(&(bdata->user));
+
+    fclose(bdata->transaction_fptr);
+}
+
+void check_account_value(User_Bank_Data* bdata) {
+    puts("\nYour accounts: ");
+    printf("Checking account: $%.2lf\n", bdata->checking_account_amount);
+    printf("Savings account: $%.2lf\n\n", bdata->savings_account_amount);
+}
+
+void withdraw(User_Bank_Data* bdata) {
+    check_account_value(bdata);
+    
+    int choice = 0;
+    while (true) {
+        puts("1.) Checking account");
+        puts("2.) Savings account");
+        puts("3.) Return");
+        printf("Withdraw from which account: ");
+
+        choice = get_int("Invaild integer: ");
+        if (choice != CHECKING && choice != SAVINGS)
+            printf("Invaid option\n");
+        else if (choice == 3)
+            return;
+        else
+            break; 
+    }
+
+    printf("Withdraw how much (Type in decimal/integer number): ");
+    double withdraw_amount = get_double();
+
+    switch (choice) {
+        case CHECKING:
+            bdata->checking_account_amount -= withdraw_amount;
+            break;
+        case SAVINGS:
+            bdata->savings_account_amount -= withdraw_amount; 
+            break;
+    }
 }
