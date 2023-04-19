b31b48fe03301e2edeebe46f1f82ea70172c6b82
Osmar Rojas <osmarrojas69@gmail.com>
2023-04-19 01:48:24 -0400

    Final verion of main.c

--- main.c
+++ main.c
@@ -1,12 +1,5 @@
-#include <stdio.h>
-#include "include/input.h"
 #include "include/menu.h"
 
 int main() {
-    // char* name = get_str();
-    // printf("%s\n", name);
-    // free(name);
-
-    // login_prompt();
-    user_creation_portal();
+    login_prompt();
 }
