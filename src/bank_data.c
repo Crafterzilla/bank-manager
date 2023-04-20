e3c4d9df567136928c4a3dcabfe9635dabb03c89
Osmar Rojas <osmarrojas69@gmail.com>
2023-04-20 09:48:29 -0400

    Remove unnessary files

--- src/bank_data.c
+++
@@ -1,7 +0,0 @@
-#include "../include/bank_data.h"
-
-void init_bank_data(Bank_data* data) {
-    data->checking_account_amount = 0;
-    data->savings_account_amount = 0;
-    data->transaction_histroy = NULL;
-}
